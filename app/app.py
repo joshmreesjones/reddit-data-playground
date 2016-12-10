9df3275ae7f2f86dcbde01bc3b5de77f8a0a5bf2
jmreesjo <jmreesjo@ncsu.edu>
2016-12-10 13:41:31 -0500

    Remove REST API

--- app/app.py
+++ app/app.py
@@ -1,19 +1,10 @@
 from flask import Flask
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
-class Stats(Resource):
-    def get(self):
-        return {"msg": "This is the Stats resource."}
+@app.route('/')
+def index():
+    return 'Hello, world!'
 
-class Recommend(Resource):
-    def get(self, subreddit):
-        return {"msg": "Here is a subreddit recommendation for %s." % subreddit}
-
-api.add_resource(Stats, "/api/stats")
-api.add_resource(Recommend, "/api/recommend/<string:subreddit>")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
