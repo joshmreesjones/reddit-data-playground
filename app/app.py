351294f9ff11186fcdf3a691fab043af6eb80ed8
jmreesjo <jmreesjo@ncsu.edu>
2016-12-12 00:02:48 -0500

    Start REST API endpoint for recommender

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/similar/<string:subreddit>')
+def test(subreddit):
+    # TODO: cache this query somewhere so it's faster in the future
+    return jsonify({"foo": subreddit})
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
